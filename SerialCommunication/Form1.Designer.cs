770e744798e55385578195502c41524591702c63
Mohammadhossein Nezhadhendi <moh.nezh1377@gmail.com>
2020-09-20 12:37:48 +0430

    simple serial communication

---
+++ SerialCommunication/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SerialCommunication
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ComboPorts = new System.Windows.Forms.ComboBox();
+            this.BtnOpen = new System.Windows.Forms.Button();
+            this.BtnClose = new System.Windows.Forms.Button();
+            this.TxtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.BtnRecieve = new System.Windows.Forms.Button();
+            this.TxtRecive = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "port:";
+            // 
+            // ComboPorts
+            // 
+            this.ComboPorts.FormattingEnabled = true;
+            this.ComboPorts.Location = new System.Drawing.Point(97, 37);
+            this.ComboPorts.Name = "ComboPorts";
+            this.ComboPorts.Size = new System.Drawing.Size(121, 21);
+            this.ComboPorts.TabIndex = 1;
+            // 
+            // BtnOpen
+            // 
+            this.BtnOpen.Location = new System.Drawing.Point(241, 37);
+            this.BtnOpen.Name = "BtnOpen";
+            this.BtnOpen.Size = new System.Drawing.Size(75, 23);
+            this.BtnOpen.TabIndex = 2;
+            this.BtnOpen.Text = "Open";
+            this.BtnOpen.UseVisualStyleBackColor = true;
+            this.BtnOpen.Click += new System.EventHandler(this.BtnOpen_Click);
+            // 
+            // BtnClose
+            // 
+            this.BtnClose.Location = new System.Drawing.Point(322, 37);
+            this.BtnClose.Name = "BtnClose";
+            this.BtnClose.Size = new System.Drawing.Size(75, 23);
+            this.BtnClose.TabIndex = 3;
+            this.BtnClose.Text = "Close";
+            this.BtnClose.UseVisualStyleBackColor = true;
+            this.BtnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // TxtSend
+            // 
+            this.TxtSend.Location = new System.Drawing.Point(97, 81);
+            this.TxtSend.Multiline = true;
+            this.TxtSend.Name = "TxtSend";
+            this.TxtSend.Size = new System.Drawing.Size(583, 118);
+            this.TxtSend.TabIndex = 4;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(605, 205);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 5;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // BtnRecieve
+            // 
+            this.BtnRecieve.Location = new System.Drawing.Point(605, 381);
+            this.BtnRecieve.Name = "BtnRecieve";
+            this.BtnRecieve.Size = new System.Drawing.Size(75, 23);
+            this.BtnRecieve.TabIndex = 7;
+            this.BtnRecieve.Text = "Recived";
+            this.BtnRecieve.UseVisualStyleBackColor = true;
+            this.BtnRecieve.Click += new System.EventHandler(this.BtnRecieve_Click);
+            // 
+            // TxtRecive
+            // 
+            this.TxtRecive.Location = new System.Drawing.Point(97, 248);
+            this.TxtRecive.Multiline = true;
+            this.TxtRecive.Name = "TxtRecive";
+            this.TxtRecive.Size = new System.Drawing.Size(583, 118);
+            this.TxtRecive.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "send";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Receive";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BtnRecieve);
+            this.Controls.Add(this.TxtRecive);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.TxtSend);
+            this.Controls.Add(this.BtnClose);
+            this.Controls.Add(this.BtnOpen);
+            this.Controls.Add(this.ComboPorts);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox ComboPorts;
+        private System.Windows.Forms.Button BtnOpen;
+        private System.Windows.Forms.Button BtnClose;
+        private System.Windows.Forms.TextBox TxtSend;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button BtnRecieve;
+        private System.Windows.Forms.TextBox TxtRecive;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ SerialCommunication/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialCommunication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            ComboPorts.Items.AddRange(ports);
+            ComboPorts.SelectedIndex = 0;
+            BtnClose.Enabled = false;
+
+        }
+
+        private void BtnOpen_Click(object sender, EventArgs e)
+        {
+            BtnOpen.Enabled = false;
+            BtnClose.Enabled = true;
+            try
+            {
+                serialPort1.PortName = ComboPorts.Text;
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+       
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine(TxtSend.Text + Environment.NewLine);
+                    TxtSend.Clear();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            BtnOpen.Enabled = true;
+            BtnClose.Enabled = false;
+            try
+            {
+                serialPort1.Close();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BtnRecieve_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    TxtRecive.Text = serialPort1.ReadExisting();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+            }
+        }
+    }
+}
